82d58198f644628be5de5ec85c883c1fb1251735
Shuvodip Das Himu <shuvodipdashimu@gmail.com>
2025-05-18 10:15:43 +0600

    steps simple instructions file

---
+++ _CLIENT_SIDE_FIREBASE_DEPLOY.js
@@ -0,0 +1,16 @@
+/* 
+// install firebase project (console.firebase.google.com) start hosting
+// one time for your computer:- npm install -g firebase-tools
+For each project
+// firebase login
+// firebase init
+// do you want to proceed? : y
+// hosting -> existing project (select using keyboard up and down)
+// what is your public directory? dist
+// single page rewrite: y
+// github build auto: n
+
+for every time you want to deploy
+// npm run build
+// firebase deploy
+*/
--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import CoffeeDetails from './components/CoffeeDetails.jsx';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import AuthProvider from './contexts/AuthProvider.jsx';
+import Users from './components/Users.jsx';
 
 const router = createBrowserRouter([
   {
@@ -44,6 +45,11 @@ const router = createBrowserRouter([
       {
         path: 'signup',
         Component: SignUp
+      },
+      {
+        path: 'users',
+        loader: ()=>fetch('http://localhost:3000/users'),
+        Component: Users
       }
     ]
   },
